2ecd4cf379f671b93c8ca69fcbd463e13a8df622
NoYouSin <102772033+NoYouSin@users.noreply.github.com>
2023-06-13 22:22:22 +0900

    불필요 클래스 제거 / 변수 제거

--- main.cpp
+++ main.cpp
@@ -46,7 +46,7 @@ bool checkCollisionCircle(const sf::CircleShape& player, const sf::CircleShape& 
 sf::CircleShape spriteToCircle(const sf::Sprite& sprite) {
     sf::FloatRect spriteBounds = sprite.getGlobalBounds();
     sf::Vector2f spritePosition = sprite.getPosition();
-    float spriteRadius = std::min(spriteBounds.width, spriteBounds.height) / 8.0f;
+    float spriteRadius = std::min(spriteBounds.width, spriteBounds.height) / 8.3f;
 
     sf::CircleShape circle(spriteRadius);
     circle.setOrigin(spriteRadius, spriteRadius);
@@ -66,6 +66,8 @@ std::vector<sf::CircleShape> spritesToCircles(const std::vector<sf::Sprite>& spr
 
     return circles;
 }
+
+
 
 int main(void)
 {
@@ -155,47 +157,41 @@ int main(void)
 
     // ---------------------------------------------------------------------------
 
-    const int gravity = 4;    //중력. 점프할때 사용
+    const int gravity = 5;    //중력. 점프할때 사용
     bool isJumping = false;    //점프 중인지
     bool isBottom = true;    //바닥에 발이 닿았는지
 
     // ---------------------------------------------------------------------------
 
+    
 
-
-    vector<Texture> treeTextures;
-    vector<string> treeImages = { "images/s1.png", "images/s2.png", "images/s3.png" };
-    for (const auto& image : treeImages) {
+    vector<Texture> objTextures;
+    vector<string> objImages = { "images/s1.png", "images/s2.png", "images/s3.png" };
+    for (const auto& image : objImages) {
         Texture texture;
         if (!texture.loadFromFile(image)) {
-            cerr << "Failed to load tree image: " << image << endl;
+            cerr << "Failed to load obj image: " << image << endl;
             return -1;
         }
-        treeTextures.push_back(texture);
+        objTextures.push_back(texture);
     }
 
-    vector<Sprite> trees;
-    for (const auto& texture : treeTextures) {
-        Sprite tree(texture);
-
-        //tree.setScale(1.0f, 1.0f);
-
-        trees.push_back(tree);
+    vector<Sprite> objs;
+    for (const auto& texture : objTextures) {
+        Sprite obj(texture);
+        //obj.setScale(1.0f, 1.0f);
+        objs.push_back(obj);
     }
 
-    const int obstacleWidth = treeTextures[0].getSize().x * 0.3f;
+    const int obstacleWidth = objTextures[0].getSize().x * 0.3f;
+    const int obstacleHeight = objTextures[0].getSize().y;
 
-    const int obstacleHeight = treeTextures[0].getSize().y;
-
-
-    vector<Position> treePositions;
+    vector<Position> objPositions;
     for (int i = 0; i < 10; i++) {
         Position position;
         position.x = Width + i * getRandomNumber(300, 800);
-
         position.y = PLAYER_Y_BOTTOM - obstacleHeight;
-
-        treePositions.push_back(position);
+        objPositions.push_back(position);
     }
 
 
@@ -211,7 +207,7 @@ int main(void)
     int score = 0; // 점수
 
     ostringstream sScore;
-    sScore << "SCORE : " << score;
+    sScore << score << " M";
 
     Text scoreText;
     scoreText.setFont(font);
@@ -263,8 +259,49 @@ int main(void)
     GameOver3.setFont(arial);
     GameOver3.setString(" Restart to press 'S' ");
 
-    int start = 0;
-    int end = 0;
+    //게임 인트로
+
+    Text Intro1;
+    Intro1.setCharacterSize(30);
+    Intro1.setFillColor(Color::White);
+    Intro1.setPosition(400, 100);
+    Intro1.setFont(arial);
+    Intro1.setString(" Input your Height / Weight ");
+
+    int userW = 0;
+
+    Text Intro2;
+    Intro2.setCharacterSize(30);
+    Intro2.setFillColor(Color::White);
+    Intro2.setPosition(400, 150);
+    Intro2.setFont(arial);
+    Intro2.setString(" Weight : " + to_string(userW) + " KG");
+
+    Text Intro3;
+    Intro3.setCharacterSize(30);
+    Intro3.setFillColor(Color::White);
+    Intro3.setPosition(400, 200);
+    Intro3.setFont(arial);
+    Intro3.setString(" E : +10 / R : +1 / D : -10 / F : -1 ");
+
+    Text Intro4;
+    Intro4.setCharacterSize(30);
+    Intro4.setFillColor(Color::White);
+    Intro4.setPosition(400, 250);
+    Intro4.setFont(arial);
+    Intro4.setString(" Next To Press G ");
+
+    int endK = 0;
+
+    Text endS;
+    endS.setCharacterSize(30);
+    endS.setFillColor(Color::White);
+    endS.setPosition(400, 450);
+    endS.setFont(arial);
+    endS.setString(" Result : " + to_string(endK) + " kcal");
+    
+    int state = 0;
+    
 
     // ---------------------------------------------------------------------------
 
@@ -272,21 +309,156 @@ int main(void)
     {
         window.clear();
 
-        if (start == 0)
+        if (state == 0)
+        {
+            sf::Clock Iclock;
+            bool keyReleasedE = true;
+            bool keyReleasedR = true;
+            bool keyReleasedD = true;
+            bool keyReleasedF = true;
+            bool keyReleasedG = true;
+            while (window.isOpen())
+            {
+                sf::Event event;
+                while (window.pollEvent(event))
+                {
+                    if (event.type == sf::Event::Closed)
+                    {
+                        window.close();
+                    }
+                }
+
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::E))
+                {
+                    if (keyReleasedE)
+                    {
+                        userW += 10;
+                        keyReleasedE = false;
+                    }
+                }
+                else
+                {
+                    keyReleasedE = true;
+                }
+
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::R))
+                {
+                    if (keyReleasedR)
+                    {
+                        userW += 1;
+                        keyReleasedR = false;
+                    }
+                }
+                else
+                {
+                    keyReleasedR = true;
+                }
+
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+                {
+                    if (keyReleasedD)
+                    {
+                        userW -= 10;
+                        keyReleasedD = false;
+                    }
+                }
+                else
+                {
+                    keyReleasedD = true;
+                }
+
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::F))
+                {
+                    if (keyReleasedF)
+                    {
+                        userW -= 1;
+                        keyReleasedF = false;
+                    }
+                }
+                else
+                {
+                    keyReleasedF = true;
+                }
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::G))
+                {
+                    if (keyReleasedG)
+                    {
+                        state = 1; // Move to the next state (end state)
+                        break;
+                        keyReleasedG = false;
+                    }
+                }
+                else
+                {
+                    keyReleasedG = true;
+                }
+
+                window.clear();
+                window.draw(Intro1);
+                Intro2.setString(" Weight : " + to_string(userW) + " KG");
+                window.draw(Intro2);
+                window.draw(Intro3);
+                window.draw(Intro4);
+                window.display();
+            }
+
+            
+        }
+
+        else if (state == 1)
         {
             window.draw(GameStart);
             window.draw(GameName);
         }
-        else if (end != 0)
+        else if (state == 3)
         {
             window.clear();
             window.draw(GameOver);
 
             sScore.str("");
-            sScore << "SCORE : " << score;
+            sScore << score << " M";
             scoreText.setString(sScore.str());
-            scoreText.setPosition(520, 250);
+            scoreText.setPosition(570, 250);
             scoreText.setFillColor(Color::Red);
+
+            if (score == 30)
+            {
+                if (40 <= userW && userW <= 49)
+                {
+                    endK = 55; // 값 할당을 위해 단일 등호(=)를 사용
+                }
+                else if (50 <= userW && userW <= 59)
+                {
+                    endK = 67;
+                }
+                else if (60 <= userW && userW <= 69)
+                {
+                    endK = 80;
+                }
+                else if (70 <= userW && userW <= 79)
+                {
+                    endK = 92;
+                }
+                else if (80 <= userW && userW <= 89)
+                {
+                    endK = 104;
+                }
+                else if (90 <= userW && userW <= 99)
+                {
+                    endK = 116;
+                }
+                else if (100 <= userW && userW <= 109)
+                {
+                    endK = 129;
+                }
+
+                endS.setCharacterSize(30);
+                endS.setFillColor(Color::White);
+                endS.setPosition(460, 400);
+                endS.setString(" Result : " + to_string(endK) + " kcal");
+
+                window.draw(endS);
+            }
 
             window.draw(scoreText);
             window.draw(GameOver2);
@@ -300,15 +472,15 @@ int main(void)
             if (Keyboard::isKeyPressed(Keyboard::S))
             {
                 // 'S' 키를 누르면 게임을 다시 시작합니다.
-                start = 0;
-                end = 0;
+                
+                state = 1;
                 score = 0;
                 clock.restart();
                 playerPos.y = PLAYER_Y_BOTTOM;
                 isBottom = true;
 
                 // 점수 텍스트를 다시 설정합니다.
-                scoreText.setString("Score: " + to_string(score));
+                scoreText.setString(to_string(score) + " M");
             }
 
             // ---------------------------------------------------------------------------
@@ -321,7 +493,7 @@ int main(void)
             scoreText.setCharacterSize(30);
             scoreText.setFillColor(Color::Black);
             //logic.
-            // jump.
+            //jump.
             if (Keyboard::isKeyPressed(Keyboard::Space)) //스페이스 입력 감지
             {
                 if (isBottom && !isJumping)    //바닥이고, 점프중이 아닐때 점프 가능
@@ -346,7 +518,7 @@ int main(void)
                 playerPos.y = PLAYER_Y_BOTTOM;
                 isBottom = true;
             }
-            if (playerPos.y <= PLAYER_Y_BOTTOM - 150)    //점프해서 우주로 가지 않도록 설정
+            if (playerPos.y <= PLAYER_Y_BOTTOM - 100)    //점프해서 우주로 가지 않도록 설정
             {
                 isJumping = false;
             }
@@ -367,28 +539,38 @@ int main(void)
             std::vector<sf::CircleShape> spritesToCircles(const std::vector<sf::Sprite>&sprites);
             bool checkCollisionCircle(const sf::CircleShape & player, const sf::CircleShape & obstacle);
 
+
             // ...
 
-            // Move trees
-            for (int i = 0; i < trees.size(); i++) {
-                trees[i].setPosition(treePositions[i].x, treePositions[i].y + 52);
-                treePositions[i].x -= 6;
+            // Move objs
+            for (int i = 0; i < objs.size(); i++) {
+                objs[i].setPosition(objPositions[i].x, objPositions[i].y + 52);
+                objPositions[i].x -= 5;
+                if (score >= 200)
+                {
+                    objPositions[i].x -= 6;
+                }
+                
 
-                if (treePositions[i].x < -obstacleWidth) {
-                    treePositions[i].x = Width + getRandomNumber(300, 800);
+                if (objPositions[i].x < -obstacleWidth) {
+                    objPositions[i].x = Width + getRandomNumber(300, 800);
                 }
 
                 // Check collision
-                if (checkCollisionCircle(spriteToCircle(playerFrames[index]), spriteToCircle(trees[i]))) {
+                if (checkCollisionCircle(spriteToCircle(playerFrames[index]), spriteToCircle(objs[i]))) {
                     // Handle collision (e.g. game over)
-                    end++;
+                    state++;
                     cout << "Game over!" << endl;
-                    // ...
+                    
+                    // Remove the obj from the vector
+                    objs.erase(objs.begin() + i);
+                    objPositions.erase(objPositions.begin() + i);
+
+
                 }
             }
 
-            // ...
-
+            
             
 
 
@@ -397,20 +579,26 @@ int main(void)
             Time elapsedTime = clock.getElapsedTime();
             if (elapsedTime.asSeconds() >= 1.0f) // 1초마다 점수 증가
             {
-                score += 1; // 점수 증가량 설정
+                score += 5; // 점수 증가량 설정
                 clock.restart();
             }
 
             // ---------------------------------------------------------------------------
 
-            
+            if (score == 30)
+            {
+                state = 3;
+
+                
+
+            }
             
             // ---------------------------------------------------------------------------
             
             // 배경 이미지 변경
             if (score >= currentBackgroundScore) {
                 currentBackgroundIndex = (currentBackgroundIndex + 1) % numBackgrounds;
-                currentBackgroundScore += 2;
+                currentBackgroundScore += 10;
                 backgroundSprite.setTexture(b[currentBackgroundIndex]);
             }
 
@@ -418,11 +606,12 @@ int main(void)
 
             
             // Set positions
-            //tree.setPosition(treePos.x, treePos.y);
+            //obj.setPosition(objPos.x, objPos.y);
             playerFrames[index].setPosition(playerPos.x, playerPos.y);
 
             // Update score text
-            scoreText.setString("Score: " + to_string(score));
+            scoreText.setString( to_string(score) + " M" );
+            
 
             // ---------------------------------------------------------------------------
             
@@ -431,8 +620,8 @@ int main(void)
             window.draw(backgroundSprite);
             window.draw(groundSprite);
             window.draw(playerFrames[index]);
-            for (const auto& tree : trees) {
-                window.draw(tree);
+            for (const auto& obj : objs) {
+                window.draw(obj);
             }
             window.draw(scoreText); // 점수 텍스트 출력
 
@@ -450,9 +639,9 @@ int main(void)
             }
             else if (e.type == Event::KeyPressed)
             {
-                if (start == 0 && e.key.code == Keyboard::A)
+                if (state == 1 && e.key.code == Keyboard::A)
                 {
-                    start++;
+                    state = 2;
                 }
             }
         }
